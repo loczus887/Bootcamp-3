634811736549ef0829a2aa4747bc5e1fc64f9e73
loczus887 <alakowalska2000@wp.pl>
2024-06-27 17:28:04 +0200

    Bootcamp 3

---
+++ src/dzien2_frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Blog from './components/Blog.vue'
+</script>
+
+<template>
+  <main class="cointainer mx-auto">
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <br />
+    <br />
+    <Blog />
+  </main>
+</template>
